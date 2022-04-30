51307fcb917c0fa134b31066bbbe9e7495fc943b
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-30 12:17:40 -0400

    commit tree 4011c7b7e1d331931f44573edd3d8c8cfb6274b9

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 6.2
+let num2 = 10
+let multiply = num1 * num2
+
+let random
+random = Math.round((Math.random() * 10))
+
+
+let num3 = 5
+let num4 = 9
+let mod = num4 % num3
+
+
+let max 
+max= Math.max(1,2,20,3)
+
